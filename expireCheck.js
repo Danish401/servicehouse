895092c11edb98303127f7ee43d948166f50b5e8
Danish401 <125354722+Danish401@users.noreply.github.com>
2025-06-11 01:00:27 +0530

    Update expireCheck.js

--- expireCheck.js
+++ expireCheck.js
@@ -1,10 +1,13 @@
-const cron = require('node-cron');
+const cron = require("node-cron");
 const User = require("./models/User");
-const sendMail = require('./sendMail');
+const sendMail = require("./utils/sendMail");
 
-cron.schedule('0 0 * * *', async () => {
+cron.schedule("0 0 * * *", async () => {
   const now = new Date();
-  const users = await User.find({ isPremium: true, premiumExpiry: { $lt: now } });
+  const users = await User.find({
+    isPremium: true,
+    premiumExpiry: { $lt: now },
+  });
   for (const user of users) {
     user.isPremium = false;
     user.premiumPlan = null;
@@ -13,9 +16,8 @@ cron.schedule('0 0 * * *', async () => {
 
     await sendMail(
       user.email,
-      'Premium Plan Expired',
+      "Premium Plan Expired",
       `Hi ${user.name}, your premium plan has expired. Please renew to continue benefits.`
     );
   }
 });
-
